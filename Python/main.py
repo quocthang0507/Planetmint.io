fd0d35ac1f5d730649bc2e71716a13df797ab13e
thanglq <quocthang_0507@yahoo.com.vn>
2023-12-04 13:35:30 +0700

    first commit

---
+++ Python/main.py
@@ -0,0 +1,17 @@
+from planetmint_driver import Planetmint
+from planetmint_driver.crypto import generate_keypair
+from ipld import marshal, multihash
+
+plntmnt = Planetmint('https://test.ipdb.io')
+alice = generate_keypair()
+tx = plntmnt.transactions.prepare(
+    operation='CREATE',
+    signers=alice.public_key,
+    assets=[
+        {'data': 
+            multihash(marshal({'message': 'Blockchain all the things!'}))
+            }   
+    ]
+)
+signed_tx = plntmnt.transactions.fulfill(tx, private_keys=alice.private_key)
+print(plntmnt.transactions.send_commit(signed_tx))
